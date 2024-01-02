1536cb6d4bd19e9852b5081ac039a4cd7bb3992f
Kareem Mahlees <kareemmahlees@gmail.com>
2024-01-02 15:33:38 +0200

    refactor: astro to react

--- apps/web/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
