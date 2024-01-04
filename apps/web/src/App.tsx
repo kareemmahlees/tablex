43a2c01d59e994ac64db18a2c327d0051bb6c651
Kareem Mahlees <kareemmahlees@gmail.com>
2024-01-04 13:35:06 +0200

    feat: add vercel analytics

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,14 @@
 import Heading from "@/components/Heading"
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 import Nav from "@/components/Nav"
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { ThemeProvider } from "./components/providers/theme-provider"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Analytics />
       <SpeedInsights />
       <main className="font-[Rubik]">
         <Nav />
