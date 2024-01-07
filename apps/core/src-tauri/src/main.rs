69c70820d207819717f8f2e77318533c2e7809f0
Kareem Mahlees <kareemmahlees@gmail.com>
2024-01-07 15:28:46 +0200

    feat: cleanup connections on app shutdown

--- apps/core/src-tauri/src/main.rs
+++ apps/core/src-tauri/src/main.rs
@@ -15,6 +15,7 @@ use row::{create_row, delete_rows, get_rows, update_row};
 use sqlx::sqlite::SqlitePool;
 use sqlx::{MySqlPool, PgPool};
 use table::{get_columns_definition, get_tables};
+use tauri::{Manager, WindowEvent};
 use tokio::sync::Mutex;
 use utils::Drivers;
 
@@ -24,6 +25,28 @@ pub struct DbInstance {
     postgres_pool: Mutex<Option<PgPool>>,
     mysql_pool: Mutex<Option<MySqlPool>>,
     driver: Mutex<Option<Drivers>>,
+}
+
+impl DbInstance {
+    async fn cleanup_connections(&self) {
+        let long_lived = self.sqlite_pool.lock().await;
+        let sqlite_pool = long_lived.as_ref();
+        if let Some(sqlite_pool) = sqlite_pool {
+            sqlite_pool.close().await
+        }
+
+        let long_lived = self.postgres_pool.lock().await;
+        let postgres_pool = long_lived.as_ref();
+        if let Some(postgres_pool) = postgres_pool {
+            postgres_pool.close().await
+        }
+
+        let long_lived = self.mysql_pool.lock().await;
+        let mysql_pool = long_lived.as_ref();
+        if let Some(mysql_pool) = mysql_pool {
+            mysql_pool.close().await
+        }
+    }
 }
 
 fn main() {
@@ -58,6 +81,15 @@ fn main() {
             create_row,
             update_row,
         ])
+        .on_window_event(move |event| match event.event() {
+            WindowEvent::Destroyed => {
+                let db_instance: tauri::State<'_, DbInstance> = event.window().state();
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                rt.block_on(db_instance.cleanup_connections());
+                rt.shutdown_background();
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
