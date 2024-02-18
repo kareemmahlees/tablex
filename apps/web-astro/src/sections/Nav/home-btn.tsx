f4fb6bc28a706bd3a5ee2cbfb1cf235575215a48
Kareem Mahlees <kareemmahlees@gmail.com>
2024-02-18 16:14:15 +0200

    feat(ui): migrate BundleSizeCard ✨

--- apps/web-astro/src/sections/Nav/home-btn.tsx
+++ apps/web-astro/src/sections/Nav/home-btn.tsx
@@ -13,7 +13,7 @@ const HomeBtn = () => {
       }
     >
       <img
-        src="/icon.svg"
+        src="/logo.svg"
         alt="icon"
         className="h-5 w-5 md:h-6 md:w-6 lg:h-7 lg:w-7 "
       />
