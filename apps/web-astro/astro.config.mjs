d585a763901b14d691befcc7184546ce12eea77c
Kareem Mahlees <kareemmahlees@gmail.com>
2024-02-18 13:16:58 +0200

    feat(ui): shadcn & Nav section ✨

--- apps/web-astro/astro.config.mjs
+++ apps/web-astro/astro.config.mjs
@@ -1,4 +1,14 @@
+import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
+import react from "@astrojs/react"
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: true
+    }),
+    react()
+  ]
+})
