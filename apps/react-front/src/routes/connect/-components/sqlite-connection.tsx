b68446865483e04eceac5141292cf5effd28efb6
Kareem Mahlees <kareemmahlees@gmail.com>
2024-03-09 13:19:13 +0200

    refactor: connect page ♻️

---
+++ apps/react-front/src/routes/connect/-components/sqlite-connection.tsx
@@ -0,0 +1,130 @@
+import {
+  createConnectionRecord,
+  establishConnection,
+  testConnection
+} from "@/commands/connection"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Drivers } from "@/lib/types"
+import { constructConnectionString, customToast } from "@/lib/utils"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useNavigate } from "@tanstack/react-router"
+import { open } from "@tauri-apps/api/dialog"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import ConnectionActions from "./connection-actions"
+
+const SQLiteConnectionForm = () => {
+  const [selectedPath, setSelectedPath] = useState<string>()
+
+  return (
+    <>
+      <Button
+        variant={"secondary"}
+        onClick={async () => {
+          const selected = await open({
+            multiple: false,
+            filters: [
+              {
+                name: "DB File",
+                extensions: ["db", "db3", "s3db", "sl3", "sqlite", "sqlite3"]
+              }
+            ]
+          })
+          setSelectedPath(selected as string)
+        }}
+      >
+        Select DB file
+      </Button>
+      {selectedPath && <ConnectionForm selectedPath={selectedPath} />}
+    </>
+  )
+}
+
+export default SQLiteConnectionForm
+
+type ConnectionFormProps = {
+  selectedPath: string
+}
+
+const formSchema = z.object({
+  connName: z.string().min(1, { message: "Connection name is required" })
+})
+
+const ConnectionForm = ({ selectedPath }: ConnectionFormProps) => {
+  const navigate = useNavigate({ from: "/connect" })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema)
+  })
+
+  const onClickConnect = async () => {
+    const connString = constructConnectionString({
+      driver: Drivers.SQLite,
+      filePath: selectedPath
+    })
+    customToast(
+      establishConnection(connString, Drivers.SQLite),
+      {
+        success: () => {
+          navigate({ to: "" }) // TODO naviaget to dashboard page
+          return "Successfully created connection"
+        },
+        error: (e: string) => e
+      },
+      "create_connection"
+    )
+  }
+
+  const onClickSave = async (values: z.infer<typeof formSchema>) => {
+    const connString = constructConnectionString({
+      driver: Drivers.SQLite,
+      filePath: selectedPath
+    })
+    await createConnectionRecord(values.connName, connString, Drivers.SQLite)
+    navigate({ to: "" }) // TODO navigate to connections page
+  }
+
+  const onClickTest = async () => {
+    const connString = constructConnectionString({
+      driver: Drivers.SQLite,
+      filePath: selectedPath
+    })
+    await testConnection(connString, Drivers.SQLite)
+  }
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8">
+        <FormField
+          control={form.control}
+          name="connName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Connection Name</FormLabel>
+              <FormControl>
+                <Input placeholder="e.g awesome project dev" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <pre className="text-muted-foreground text-sm">{selectedPath}</pre>
+
+        <ConnectionActions
+          onClickConnect={form.handleSubmit(onClickConnect)}
+          onClickSave={form.handleSubmit(onClickSave)}
+          onClickTest={form.handleSubmit(onClickTest)}
+        />
+      </form>
+    </Form>
+  )
+}
--- apps/react-front/vite.config.ts
+++ apps/react-front/vite.config.ts
@@ -1,8 +1,14 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 import react from "@vitejs/plugin-react"
+import path from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()]
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
