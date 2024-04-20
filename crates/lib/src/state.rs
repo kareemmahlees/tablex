32fdf418420173e7e8e0d714cf8faa4c6e74572a
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-04-20 22:08:26 +0200

    chore: some cleanup ♻️

--- crates/lib/src/state.rs
+++ crates/lib/src/state.rs
@@ -1,34 +1,20 @@
 use crate::handler::Handler;
-use crate::Drivers;
-use sqlx::sqlite::SqlitePool;
-use sqlx::{AnyPool, MySqlPool, PgPool};
+use sqlx::AnyPool;
 #[cfg(not(debug_assertions))]
 use tauri::api::process::CommandChild;
 
 #[derive(Default, Debug)]
 pub struct SharedState {
-    pub sqlite_pool: Option<SqlitePool>,
-    pub postgres_pool: Option<PgPool>,
-    pub mysql_pool: Option<MySqlPool>,
     pub handler: Option<Box<dyn Handler>>,
     pub pool: Option<AnyPool>,
-    pub driver: Option<Drivers>,
     #[cfg(not(debug_assertions))]
     pub metax: Option<CommandChild>,
 }
 
 impl SharedState {
     pub async fn cleanup(&mut self) {
-        if let Some(sqlite_pool) = &self.sqlite_pool {
-            sqlite_pool.close().await
-        }
-
-        if let Some(pg_pool) = &self.postgres_pool {
-            pg_pool.close().await
-        }
-
-        if let Some(mysql_pool) = &self.mysql_pool {
-            mysql_pool.close().await
+        if let Some(pool) = &self.pool {
+            pool.close().await
         }
 
         #[cfg(not(debug_assertions))]
