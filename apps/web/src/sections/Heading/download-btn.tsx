9bb360c612bd625b01205d82986b7e2f9771bc20
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-06-10 21:29:07 +0300

    feat: os type in download type ✨

---
+++ apps/web/src/sections/Heading/download-btn.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button"
+
+const DownloadBtn = () => {
+  const userAgent = window.navigator.userAgent
+  let iconSrc: string
+  if (userAgent.includes("Windows")) {
+    iconSrc = "https://cdn.simpleicons.org/windows10/black"
+  } else if (userAgent.includes("Mac")) {
+    iconSrc = "https://cdn.simpleicons.org/macos/black"
+  } else if (userAgent.includes("Linux")) {
+    iconSrc = "https://cdn.simpleicons.org/linux/black"
+  } else {
+    iconSrc = ""
+  }
+
+  return (
+    <Button
+      className="space-x-3"
+      onClick={() => {
+        document.getElementById("download")?.scrollIntoView({ block: "center" })
+      }}
+    >
+      <img src={iconSrc} alt="os_icon" className="h-4 w-4" />
+      <p>Download</p>
+    </Button>
+  )
+}
+
+export default DownloadBtn
