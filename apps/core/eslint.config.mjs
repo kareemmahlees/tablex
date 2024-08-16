a5cb8d72e5dc8a41475a3e01505aee48e3e0f5e2
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-08-16 21:46:57 +0300

    perf: fix a perf issue in pre commit hooks ⚡️

--- apps/core/eslint.config.mjs
+++ apps/core/eslint.config.mjs
@@ -1,44 +1,53 @@
-import { fixupConfigRules } from "@eslint/compat";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat"
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import tsParser from "@typescript-eslint/parser"
+import reactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-export default [{
-    ignores: ["**/dist", "**/.eslintrc.cjs", "src/components/ui", "src/bindings.ts"],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-)), {
+export default [
+  {
+    ignores: ["**/dist", "src/components/ui", "src/bindings.ts"]
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended"
+    )
+  ),
+  {
     plugins: {
-        "react-refresh": reactRefresh,
+      "react-refresh": reactRefresh
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser
+      },
 
-        parser: tsParser,
+      parser: tsParser
     },
 
     rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true
+        }
+      ],
 
-        "@typescript-eslint/no-explicit-any": "off",
-    },
-}];
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
+]
