0a24d6d2953e53104cab19dc11edbaf895595c8b
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-08-26 22:48:07 +0300

    build: add eslint for website workspace

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,25 @@
+import pluginJs from "@eslint/js"
+import react from "eslint-plugin-react"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ...react.configs.flat.recommended,
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
+  { ignores: ["tailwind.config.ts", "src/components", "src/env.d.ts"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
+]
