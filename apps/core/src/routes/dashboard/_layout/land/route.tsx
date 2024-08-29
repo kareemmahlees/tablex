4d15d9932c1f3169eafc17e1b242a90cb0ef670b
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-08-29 20:16:38 +0300

    chore: remove unused assets 🍱

--- apps/core/src/routes/dashboard/_layout/land/route.tsx
+++ apps/core/src/routes/dashboard/_layout/land/route.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/dashboard/_layout/land")({
 function DashboardPage() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-y-3 break-words text-center text-2xl font-bold text-gray-500 opacity-50">
-      <img src={"/cube.svg"} alt="cube" width={100} height={100} />
+      <img src={"/icons/cube.svg"} alt="cube" width={100} height={100} />
       Choose a table
       <br />
       to get started
