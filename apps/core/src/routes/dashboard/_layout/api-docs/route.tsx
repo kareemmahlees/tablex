185080019b425cc7e2ff6fb6af3950bd3006e9e1
Kareem Ebrahim <kareemmahlees@gmail.com>
2025-03-16 00:55:31 +0200

    chore: fix formatting

--- apps/core/src/routes/dashboard/_layout/api-docs/route.tsx
+++ apps/core/src/routes/dashboard/_layout/api-docs/route.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/dashboard/_layout/api-docs')({
+export const Route = createFileRoute("/dashboard/_layout/api-docs")({
   component: () => <div>Hello /dashboard/_layout/api-docs!</div>
 })
