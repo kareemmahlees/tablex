fafad5789225b29a6e4d76986ab98f8920893ae4
Kareem Ebrahim <kareemmahlees@gmail.com>
2025-04-09 17:23:07 +0200

    feat(WIP): latest virtualization work

--- apps/core/src/components/custom/data-table.tsx
+++ apps/core/src/components/custom/data-table.tsx
@@ -1,6 +1,8 @@
 import type { ColumnInfo } from "@/bindings"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { flexRender, Table as TanstackTable } from "@tanstack/react-table"
+import { useVirtualizer } from "@tanstack/react-virtual"
+import { useRef } from "react"
 import { ScrollBar } from "../ui/scroll-area"
 import {
   Table,
@@ -12,8 +14,21 @@ import {
 } from "../ui/table"
 
 export const DataTable = ({ table }: { table: TanstackTable<ColumnInfo> }) => {
+  const { rows } = table.getCoreRowModel()
+
+  const parentRef = useRef<HTMLDivElement>(null)
+
+  console.log("count", rows.length)
+
+  const virtualizer = useVirtualizer({
+    count: rows.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 5,
+    debug: import.meta.env.DEV
+  })
+
   return (
-    <ScrollArea className="relative h-full w-full">
+    <ScrollArea className="relative h-full w-full" ref={parentRef}>
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -40,7 +55,36 @@ export const DataTable = ({ table }: { table: TanstackTable<ColumnInfo> }) => {
           ))}
         </TableHeader>
         <TableBody>
-          {table.getRowModel().rows?.length ? (
+          {virtualizer.getVirtualItems().map((virtualRow, index) => {
+            const row = rows[virtualRow.index]
+            return (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+                data-id={row.id}
+                className="hover:bg-muted/70 data-[state=selected]:bg-muted/70 transition-colors"
+                style={{
+                  height: `${virtualRow.size}px`,
+                  transform: `translateY(${
+                    virtualRow.start - index * virtualRow.size
+                  }px)`
+                }}
+              >
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            )
+          })}
+
+          {/* {table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row) => (
               <TableRow
                 key={row.id}
@@ -63,7 +107,7 @@ export const DataTable = ({ table }: { table: TanstackTable<ColumnInfo> }) => {
                 No results.
               </TableCell>
             </TableRow>
-          )}
+          )} */}
         </TableBody>
       </Table>
       <ScrollBar orientation="horizontal" />
