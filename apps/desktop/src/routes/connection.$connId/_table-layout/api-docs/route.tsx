154eef6a025357214bdd80a52b9a6137283eb051
Kareem Ebrahim <kareemmahlees@gmail.com>
2025-08-31 17:31:01 +0300

    feat(ui): setup utilities route

--- apps/desktop/src/routes/connection.$connId/_table-layout/api-docs/route.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router"
-
-export const Route = createFileRoute(
-  "/connection/$connId/_table-layout/api-docs"
-)({
-  component: () => <div>Hello /dashboard/_layout/api-docs!</div>
-})
